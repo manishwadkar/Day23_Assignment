46375c2d9450fac57e131b3edb2a77cc3c5b7a53
“manishwadkar” <wadkarmanish@gmail.com>
2020-08-03 21:49:56 +0530

    UC5 implemented

--- src/EmpWageBuilder.java
+++ src/EmpWageBuilder.java
@@ -1,31 +1,36 @@
-
 public class EmpWageBuilder{
 
         public static final int isFullTime=1;
         public static final int isPartTime=2;
         public static final int wagePerHour=20;
+        public static final int noOfWorkingDays=20;
 
         public static void main(String args[]){
                 System.out.println("Welcome to Employee Wage Computation");
                 int empHours=0;
                 int empWage=0;
+                int totalEmpWage=0;
 
-                int empCheck=(int)Math.floor(Math.random() * 10) % 3;
-                System.out.println(empCheck);
+                for(int i=0;i<noOfWorkingDays;i++){
+                        int empCheck=(int)Math.floor(Math.random() * 10) % 3;
+                        System.out.println(empCheck);
 
-                switch (empCheck){
+                        switch (empCheck){
 
-                case isFullTime:
-                        empHours=8;
-                        break;
-                case isPartTime:
-                        empHours=4;
-                        break;
-                default:
-                        empHours=0;
+                        case isFullTime:
+                                empHours=8;
+                                break;
+                        case isPartTime:
+                                empHours=4;
+                                break;
+                        default:
+                                empHours=0;
+                        }
+                        empWage=empHours*wagePerHour;
+                        System.out.println("Employee Wage: "+empWage);
+                        totalEmpWage+=empWage;
                 }
-                empWage=empHours*wagePerHour;
-                System.out.println("Employee Wage: "+empWage);
+                System.out.println("Total Employee Wage: "+totalEmpWage);
         }
 }
 
