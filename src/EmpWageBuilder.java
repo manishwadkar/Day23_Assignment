8ea41737f4a6a3cd03cf0a9ebb5641e604b5b1a1
“manishwadkar” <wadkarmanish@gmail.com>
2020-08-03 21:39:41 +0530

    Added UC2-CheckEmpAttendance

--- src/EmpWageBuilder.java
+++ src/EmpWageBuilder.java
@@ -1,15 +1,19 @@
-
 public class EmpWageBuilder{
 
         public static void main(String args[]){
                 System.out.println("Welcome to Employee Wage Computation");
                 int isFullTime=1;
-                double empCheck=Math.floor(Math.random() * 10) % 2;
+                int wagePerHour=20;
+                int empHours=0;
+                int empWage=0;
+
+                int empCheck=(int)Math.floor(Math.random() * 10) % 2;
                 System.out.println(empCheck);
                 if(empCheck == isFullTime)
-                        System.out.println("Employee is Present");
+                        empHours=8;
                 else
-                        System.out.println("Employee is Absent");
+                        empHours=0;
+                empWage=empHours*wagePerHour;
+                System.out.println("Employee Wage: "+empWage);
         }
 }
-
