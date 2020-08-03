fc78d692403bdad8f31f4b486b2d61ec9c40e95c
“manishwadkar” <wadkarmanish@gmail.com>
2020-08-03 21:34:55 +0530

    Added UC1-CheckEmpAttendance to UC1 branch

---
+++ src/EmpWageBuilder.java
@@ -0,0 +1,15 @@
+
+public class EmpWageBuilder{
+
+        public static void main(String args[]){
+                System.out.println("Welcome to Employee Wage Computation");
+                int isFullTime=1;
+                double empCheck=Math.floor(Math.random() * 10) % 2;
+                System.out.println(empCheck);
+                if(empCheck == isFullTime)
+                        System.out.println("Employee is Present");
+                else
+                        System.out.println("Employee is Absent");
+        }
+}
+
