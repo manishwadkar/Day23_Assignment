df33c68f1ef939067ae527ceb7aeeb6fd0997522
“manishwadkar” <wadkarmanish@gmail.com>
2020-08-03 22:20:50 +0530

    UC9 implemented

--- src/EmpWageBuilder.java
+++ src/EmpWageBuilder.java
@@ -4,10 +4,22 @@ public class EmpWageBuilder{
         public static final int isFullTime=1;
         public static final int isPartTime=2;
 
-        public static int computeEmpWage(String company,int wagePerHour,int noOfWorkingDays,int maxHoursInMonth){
+        private final String company;
+        private final int wagePerHour;
+        private final int noOfWorkingDays;
+        private final int maxHoursInMonth;
+        private int totalEmpWage;
+        
+        public EmpWageBuilder(String company,int wagePerHour,int noOfWorkingDays,int maxHoursInMonth) {
+        	this.company=company;
+        	this.wagePerHour=wagePerHour;
+        	this.noOfWorkingDays=noOfWorkingDays;
+        	this.maxHoursInMonth=maxHoursInMonth;
+        }
+        
+        public void computeEmpWage(){
                 System.out.println("Welcome to Employee Wage Computation");
                 int empHours=0;
-                int totalEmpWage=0;
                 int totalEmpHrs=0;
                 int totalWorkingDays=0;
 
@@ -31,14 +43,18 @@ public class EmpWageBuilder{
                         System.out.println("Day: "+totalWorkingDays+"EmpHrs: "+empHours);
                 }
                 totalEmpWage=totalEmpHrs*wagePerHour;
-                System.out.println("Total Employee Wage for Company: "+company+" is: "+totalEmpWage);
-                return totalEmpWage;
         }
 
+        public String toString() {
+        	return "Total Employee Wage for Company: "+company+" is: "+totalEmpWage;
+        }
         public static void main(String args[]){
 
-                computeEmpWage("Google",20,20,50);
-                computeEmpWage("Microsoft",50,15,30);
-                computeEmpWage("Facebook",10,4,20);
+        	EmpWageBuilder g=new EmpWageBuilder("Google", 20, 20, 50);
+        	EmpWageBuilder m=new EmpWageBuilder("Microsoft", 50, 15, 30);
+        	g.computeEmpWage();
+        	System.out.println(g);
+        	m.computeEmpWage();
+        	System.out.println(m);
         }
 }
